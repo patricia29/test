3fc72445876756b87a0ec4b224c6ef7314cdf4b7
pribeiroduarte <6?PnJ4$vuvBFO!!>
2021-11-29 16:47:44 +0000

    correct main activity

--- app/src/main/java/com/example/bookstore/MainActivity.kt
+++ app/src/main/java/com/example/bookstore/MainActivity.kt
@@ -2,12 +2,8 @@ package com.example.bookstore
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.bookstore.repository.BookRepository
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
-class MainActivity(private val repository: BookRepository) : AppCompatActivity() {
+class MainActivity() : AppCompatActivity() {
 
     /**
      * OnCreate
